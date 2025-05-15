4ebaa73f3604c12d76799050a478c9a7f311a03f
Luca Rohrer <rohrer.luca@gmail.com>
2025-05-15 10:22:55 +0200

    Refactoring

--- helper/create_csv_utils.py
+++ helper/create_csv_utils.py
@@ -1,3 +1,4 @@
+import ipaddress
 import os
 
 import pandas as pd
@@ -13,46 +14,51 @@ def create_csv(packets, filename='packets_export.csv', append=False):
     :param append: If True, appends the packets to a CSV file
     """
     data = []
-
+    count = 0
     for frame_num, pkt in enumerate(packets):
+        count += 1
         row = {
             'frame_num': frame_num,
             'old_frame_num': pkt.old_frame_number,
             'timestamp': pkt.time,
-            # 'src_ip': int(ipaddress.IPv4Address(pkt[IP].src)) if IP in pkt else 0,
-            # 'dst_ip': int(ipaddress.IPv4Address(pkt[IP].dst)) if IP in pkt else 0,
-            # 'ip_ihl': pkt[IP].ihl if IP in pkt else -1,
-            # 'ip_tos': pkt[IP].tos if IP in pkt else -1,
-            # 'ip_len': pkt[IP].len if IP in pkt else -1,
-            # 'ip_id': pkt[IP].id if IP in pkt else -1,
-            # 'ip_flags': pkt[IP].flags.value if IP in pkt else 0,
-            # 'ip_frag': pkt[IP].frag if IP in pkt else -1,
-            # 'ip_ttl': pkt[IP].ttl if IP in pkt else -1,
-            # 'ip_proto': pkt[IP].proto if IP in pkt else -1,
+            'src_ip': int(ipaddress.IPv4Address(pkt[IP].src)) if IP in pkt else 0,
+            'dst_ip': int(ipaddress.IPv4Address(pkt[IP].dst)) if IP in pkt else 0,
+            'ip_ihl': pkt[IP].ihl if IP in pkt else -1,
+            'ip_tos': pkt[IP].tos if IP in pkt else -1,
+            'ip_len': pkt[IP].len if IP in pkt else -1,
+            'ip_id': pkt[IP].id if IP in pkt else -1,
+            'ip_flags': pkt[IP].flags.value if IP in pkt else 0,
+            'ip_frag': pkt[IP].frag if IP in pkt else -1,
+            'ip_ttl': pkt[IP].ttl if IP in pkt else -1,
+            'ip_proto': pkt[IP].proto if IP in pkt else -1,
             # TCP fields
-            # 'src_port': pkt[TCP].sport if TCP in pkt else -1,
-            # 'dst_port': pkt[TCP].dport if TCP in pkt else -1,
-            # 'tcp_seq': pkt[TCP].seq if TCP in pkt else -1,
-            # 'tcp_ack': pkt[TCP].ack if TCP in pkt else -1,
-            # 'tcp_dataofs': pkt[TCP].dataofs if TCP in pkt else -1,
-            # 'tcp_flags': pkt[TCP].flags.value if TCP in pkt else 0,
-            # 'tcp_window': pkt[TCP].window if TCP in pkt else -1,
-            # 'tcp_urgptr': pkt[TCP].urgptr if TCP in pkt else -1,
+            'src_port': pkt[TCP].sport if TCP in pkt else -1,
+            'dst_port': pkt[TCP].dport if TCP in pkt else -1,
+            'tcp_seq': pkt[TCP].seq if TCP in pkt else -1,
+            'tcp_ack': pkt[TCP].ack if TCP in pkt else -1,
+            'tcp_dataofs': pkt[TCP].dataofs if TCP in pkt else -1,
+            'tcp_flags': pkt[TCP].flags.value if TCP in pkt else 0,
+            'tcp_window': pkt[TCP].window if TCP in pkt else -1,
+            'tcp_urgptr': pkt[TCP].urgptr if TCP in pkt else -1,
             # UDP fields
-            # 'udp_sport': pkt[UDP].sport if UDP in pkt else -1,
-            # 'udp_dport': pkt[UDP].dport if UDP in pkt else -1,
-            # 'udp_len': pkt[UDP].len if UDP in pkt else -1,
+            'udp_sport': pkt[UDP].sport if UDP in pkt else -1,
+            'udp_dport': pkt[UDP].dport if UDP in pkt else -1,
+            'udp_len': pkt[UDP].len if UDP in pkt else -1,
             # Payload
-            # 'payload_size': len(pkt[Raw].load) if Raw in pkt else 0,
-            'payload_bytes': ','.join(str(b) for b in pkt[Raw].load) if Raw in pkt else '',
+            'payload_size': len(pkt[Raw].load) if Raw in pkt else 0,
+            'payload_bytes': ','.join(str(b) for b in pkt[Raw].load) if Raw in pkt else str([0]),
             'prediction': pkt.prediction
         }
         data.append(row)
 
     df = pd.DataFrame(data)
+    print(f'Number of entries {count}')
     if append and os.path.exists(filename):
         df.to_csv(filename, mode='a', header=False, index=False)
         print(f"Data appended to existing CSV file '{filename}'.")
     else:
+        if not append and os.path.exists(filename):
+            os.remove(filename)
+            print(f"Existing file '{filename}' was deleted.")
         df.to_csv(filename, index=False)
         print(f"CSV file '{filename}' has been created.")
--- ml/cnn_scenario_one.py
+++ ml/cnn_scenario_one.py
@@ -14,24 +14,25 @@ from helper.ml import undersample_df
 from helper.preprocessing_cnn_helper import PreprocessingCnnHelper
 
 np.random.seed(42)
+# Read data
 df = pd.read_csv('../cnn.csv')
 df_corr = pd.read_csv('../cnn.csv')
 df['payload_bytes'] = df['payload_bytes'].apply(ast.literal_eval)
-
 df = PreprocessingCnnHelper.split_payload_bytes(df, payload_col='payload_bytes', prefix='byte', fill_value=0)
+# Print correlation
 print(df_corr.corr(numeric_only=True)['prediction'].sort_values(ascending=False))
-
 # Take data to check
-num_manual_samples_per_class = 10
+num_manual_samples_per_class = 100
 df_0 = df[df['prediction'] == 0].sample(n=num_manual_samples_per_class)
 df_1 = df[df['prediction'] == 1].sample(n=num_manual_samples_per_class)
 df_manual_test = pd.concat([df_0, df_1])
 df = df.drop(df_manual_test.index)
-
+# Prevent a dominant class
 df = undersample_df(df, target_col='prediction')
+print(f'Einträge: {len(df)}')
+# Split Dataset to x and y
 y = df.pop('prediction').values
 X = df.drop(columns=['payload_bytes', 'timestamp', 'frame_num', 'old_frame_num']).values
-
 # Scale features
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
@@ -50,7 +51,6 @@ model = models.Sequential([
     ),
     layers.BatchNormalization(),
     layers.Activation('relu'),
-    layers.Dropout(0.6),
     layers.MaxPooling1D(pool_size=2),
     layers.Conv1D(
         filters=32,
@@ -60,14 +60,14 @@ model = models.Sequential([
     ),
     layers.BatchNormalization(),
     layers.Activation('relu'),
-    layers.Dropout(0.6),
+
     layers.GlobalMaxPooling1D(),
     layers.Dense(
         units=64,
         activation='relu',
         kernel_regularizer=regularizers.l2(0.008)
     ),
-    layers.Dropout(0.6),
+    layers.Dropout(0.3),
     layers.Dense(1, activation='sigmoid')
 ])
 model.compile(optimizer='adam',
@@ -91,7 +91,7 @@ early_stopping = EarlyStopping(
     min_delta=1e-4,
     verbose=1
 )
-# Training
+#Training
 history = model.fit(
     X_train, y_train,
     epochs=1000,
@@ -99,12 +99,13 @@ history = model.fit(
     validation_split=0.1,
     callbacks=[tensorboard_callback, reduce_lr, early_stopping]
 )
-# Save model
+
+#Save model
 os.makedirs("saved_model", exist_ok=True)
 model.save("saved_model/cnn_model.keras")
+# Evaluate model
 loss, acc = model.evaluate(X_test, y_test)
 print(f"Test Loss: {loss:.4f}, Test Accuracy: {acc:.4f}")
-
 
 def predict_packet(df_single):
     x = df_single.values
@@ -112,19 +113,16 @@ def predict_packet(df_single):
     x_cnn = x_scaled.reshape((1, x_scaled.shape[1], 1))
     prob = model.predict(x_cnn)[0, 0]
     return 1 if prob > 0.5 else 0
-
-
 correct = 0
 total = len(df_manual_test)
 
 for i in range(total):
     df_sample = \
-        df_manual_test.drop(columns=['payload_bytes', 'timestamp', 'frame_num', 'old_frame_num', 'prediction']).iloc[
-            [i]]
+        df_manual_test.drop(columns=['payload_bytes', 'timestamp', 'frame_num', 'old_frame_num', 'prediction']).iloc[[i]]
     true_label = df_manual_test['prediction'].iloc[i]
     pred_label = predict_packet(df_sample)
-    print(f"{i + 1}: Echte Klasse = {true_label}, Vorhergesagt = {pred_label}")
+    print(f"{i + 1}: True label = {true_label}, Predicted = {pred_label}")
     if pred_label == true_label:
         correct += 1
-accuracy_percent = 100 * correct / total
+accuracy_percent = (100 * correct) / total
 print(f"Test-Accuracy: {accuracy_percent:.2f}% ({correct}/{total} korrekt)")
